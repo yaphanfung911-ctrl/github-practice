a16b1af33d3f299b8d6e3b817942b4d0a54f02d4
yaphanfung911-ctrl <yaphanfung911@gmail.com>
2026-06-30 16:00:19 +0900

    Update pythonpractice.py

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,8 +1,47 @@
 #問題1（ウォーミングアップ）
-name="YAP";
-print("Hello," + name + "!");
+name="YAP"
+print("Hello," + name + "!")
 
 #問題2（四則演算）
 a=18
 b=7
-print(a+b\n a-b\n a*b\n mod(a/b);
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+
+#問題3（条件分岐）
+score=75
+if(score>=80)
+  print("Pass")
+else:
+  print("Fail")
+
+#問題4(for文)
+for i in range(1,11):
+  print(i)
+
+#問題５（リスト）
+nums=[3,5,7,9]
+print(sum(nums))
+
+#問題6（文字列）
+text="docomo"
+print(len(text))
+
+#問題7（関数）
+def square(x):
+return x * x
+
+print(square(5))
+
+#問題8
+nums=[15,8,22,9,30]
+print(max(nums))
+
+
+
+
+
+
+  
